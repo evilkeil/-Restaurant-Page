b717d47991cc918b89b83410c08e0ae47ccf0c06
evilkeil <114153280+evilkeil@users.noreply.github.com>
2023-09-03 15:42:58 +0900

    install html webpack plug in and create a template to test how the page would look like

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
@@ -32,4 +33,11 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Spice Isle Delights',
+      filename:'index.html',
+      template:'./src/template.html',
+    }),
+  ],
 };
