3ed229698bab02e0e37d81e37c159d245f2910dd
evilkeil <114153280+evilkeil@users.noreply.github.com>
2023-09-04 13:48:09 +0900

    add images create a new dir in assests for images add images into it

--- src/index.js
+++ src/index.js
@@ -6,4 +6,4 @@ import homePageComponent from '/src/modules/about.js';
 
   
   document.body.appendChild(HeaderComponent());
-  document.body.appendChild(homePageComponent());
+  // document.body.appendChild(homePageComponent());   //commented out so that i can see the other page
