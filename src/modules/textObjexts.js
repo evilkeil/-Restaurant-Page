0253ef7e54d8abd34dad95e97a4bcfc0d6e4eaa5
evilkeil <114153280+evilkeil@users.noreply.github.com>
2023-09-05 14:15:23 +0900

    create a file to gold the names and strings and export

---
+++ src/modules/textObjexts.js
@@ -0,0 +1,15 @@
+export const menuItems = {
+    item1:{
+        name:"Sri Lankan Curry Platter",
+        description:"Indulge in a symphony of flavors with our signature Sri Lankan Curry Platter. This generous platter includes your choice of protein (chicken, beef, or tofu), gently simmered in a fragrant blend of coconut milk, curry leaves, and an array of traditional spices. Served with steamed rice, coconut sambol, and papadum, this dish offers a harmonious balance of sweet, savory, and spicy notes that will transport your taste buds straight to the beaches of Sri Lanka."
+    },
+    item2:{
+        name:"Jaffna Seafood Feast",
+        description:"Dive into the ocean of flavors with our Jaffna Seafood Feast. This culinary masterpiece features a medley of fresh seafood, including succulent prawns, tender squid, and flaky fish, marinated in a fiery Jaffna-style curry sauce. Served with aromatic basmati rice and a side of tangy mango chutney, this dish captures the essence of Sri Lanka's coastal cuisine, known for its bold spices and irresistible seafood."
+    },
+    item3:{
+        name:"Kottu Roti Fusion",
+        description:"Experience the rhythmic clatter of the griddle as our chefs prepare the beloved Kottu Roti Fusion. This street food sensation combines shredded roti bread, your choice of chicken or vegetables, and an assortment of spices, all stir-fried to perfection. Topped with a fried egg and served with a side of spicy onion gravy, this dish offers a delightful fusion of textures and flavors that will leave you craving for more."
+    }
+
+}
