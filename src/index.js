3dca385ce9c982791de2a193fb723009f890c85a
evilkeil <114153280+evilkeil@users.noreply.github.com>
2023-09-06 11:01:48 +0900

    create a method to toggle page

--- src/index.js
+++ src/index.js
@@ -9,6 +9,8 @@ import toggleBtn from '/src/modules/buttonEventListerners.js';
 
   
   document.body.appendChild(HeaderComponent());
+  document.body.appendChild(homePageComponent());
+
 
   const navEventListners = function(){
       const navbtns = document.querySelector('#navbar ul');
@@ -17,14 +19,45 @@ import toggleBtn from '/src/modules/buttonEventListerners.js';
       const contactLi = document.querySelector('#navbar li:nth-child(3)');
       const btns = [homeLi,menuLi,contactLi];
 
+    function hasChild(){
+      const doesntHavePageOne = !parent.querySelector('.container') !== null;
+      const doesntHavePageTwo = parent.querySelector('.menu') !== null;
+      const doesntHavePageThree = parent.querySelector('.contact') !== null;
+
+      return{
+        doesntHavePageOne,
+        doesntHavePageTwo,
+        doesntHavePageThree
+      }
+    }
+    function togglePage(){
+      const check = hasChild();
+      if(homeLi.dataset.selected === "true"){
+        if(check.doesntHavePageOne){
+          document.body.appendChild(homePageComponent());
+        }
+      }else if(menuLi.dataset.selected === "true"){
+        if(check.doesntHavePageTwo){
+          document.body.appendChild(menuPageComponent());
+        }
+      }else if(contactLi.dataset.selected === "true"){
+        if(check.doesntHavePageThree){
+          document.body.appendChild(contactPageComponent());
+        }
+      }
+    };
+    
       btns.forEach((btn)=>{
         btn.addEventListener('click',(e)=>{
           toggleBtn(e,homeLi,menuLi,contactLi,navbtns);
+          togglePage()
         })
 
       })
+
+
     }();
       
   // document.body.appendChild(homePageComponent());   //commented out so that i can see the other page
   // document.body.appendChild(menuPageComponent()); //commented out so that i can see the other page
-  document.body.appendChild(contactPageComponent());
+  // document.body.appendChild(contactPageComponent());
